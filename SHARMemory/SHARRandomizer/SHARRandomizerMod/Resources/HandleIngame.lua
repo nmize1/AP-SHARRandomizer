0eb03d51b18c73a7d92e6101eea550a21997e828
Proddy <josh@zhbot.org>
2025-07-13 06:44:47 +0100

    Fix log breaking on interiors

--- SHARMemory/SHARRandomizer/SHARRandomizerMod/Resources/HandleIngame.lua
+++ SHARMemory/SHARRandomizer/SHARRandomizerMod/Resources/HandleIngame.lua
@@ -25,24 +25,24 @@ GroupChunk:AddChunk(P3D.FrontendPure3DObjectP3DChunk("RewardBG", 1, {X = 29, Y =
 GroupChunk:AddChunk(P3D.FrontendPure3DObjectP3DChunk("RewardFG", 1, {X = 29, Y = 145}, {X = 400, Y = 300}, {X = P3D.FrontendPure3DObjectP3DChunk.Justifications.Left, Y = P3D.FrontendPure3DObjectP3DChunk.Justifications.Top}, {A=255,R=255,G=255,B=255}, 0, 0, "rewardfg"))
 GroupChunk:AddChunk(P3D.FrontendPure3DObjectP3DChunk("PreviewWindow", 1, {X = 29, Y = 145}, {X = 400, Y = 300}, {X = P3D.FrontendPure3DObjectP3DChunk.Justifications.Left, Y = P3D.FrontendPure3DObjectP3DChunk.Justifications.Top}, {A=255,R=255,G=255,B=255}, 0, 0, "3Dmodel"))
 
-local LetterBox
+local Hud
 local PauseChunk
 for chunk in FrontendProjectChunk:GetChunks(P3D.Identifiers.Frontend_Page) do
-	if chunk.Name == "LetterBox.pag" then
-		LetterBox = chunk
+	if chunk.Name == "Hud.pag" then
+		Hud = chunk
 		break
 	end
 end
 
 if GetSetting("APLog") then
-	if LetterBox then
+	if Hud then
 		P3DFile:AddChunk(FontChunk, 1)
 	
 		local TextStyleChunk = P3D.FrontendTextStyleResourceP3DChunk:new(FontName, 1, "fonts\\" .. FontName .. ".p3d", FontName)
-		LetterBox:AddChunk(TextStyleChunk, 1)
+		Hud:AddChunk(TextStyleChunk, 1)
 	
-		local LayerChunk = LetterBox:GetChunk(P3D.Identifiers.Frontend_Layer)
-		local MultiTextChunk = P3D.FrontendMultiTextP3DChunk:new("APLog", 17, {X = 20, Y = 80}, {X = LetterBox.Resolution.X - 40, Y = math.floor(LetterBox.Resolution.Y / 4)}, {X = P3D.FrontendMultiTextP3DChunk.Justifications.Left, Y = P3D.FrontendMultiTextP3DChunk.Justifications.Top}, {A=255,R=255,G=255,B=255}, 0, 0, FontName, 1, {A=192,R=0,G=0,B=0}, {X = 2, Y = -2}, 0)
+		local LayerChunk = Hud:GetChunk(P3D.Identifiers.Frontend_Layer)
+		local MultiTextChunk = P3D.FrontendMultiTextP3DChunk:new("APLog", 17, {X = 0, Y = 80}, {X = Hud.Resolution.X - 40, Y = math.floor(Hud.Resolution.Y / 4)}, {X = P3D.FrontendMultiTextP3DChunk.Justifications.Left, Y = P3D.FrontendMultiTextP3DChunk.Justifications.Top}, {A=255,R=255,G=255,B=255}, 0, 0, FontName, 1, {A=192,R=0,G=0,B=0}, {X = 2, Y = -2}, 0)
 		local TextChunk = P3D.FrontendStringTextBibleP3DChunk:new("srr2", "APLog")
 		MultiTextChunk:AddChunk(TextChunk)
 		LayerChunk:AddChunk(MultiTextChunk)
