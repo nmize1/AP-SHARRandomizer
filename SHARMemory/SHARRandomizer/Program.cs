91aac033f400bb07ef733fa609e06704fd72c7e6
nmize1 <50761210+nmize1@users.noreply.github.com>
2025-07-25 21:14:22 -0400

    bump ver number

--- SHARMemory/SHARRandomizer/Program.cs
+++ SHARMemory/SHARRandomizer/Program.cs
@@ -3,7 +3,7 @@ using SHARRandomizer;
 using SHARRandomizer.Classes;
 using System.Diagnostics;
 
-string VERSION = "Beta 0.1.1";
+string VERSION = "Beta 0.1.2";
 
 Console.Title = $"SHAR AP Version {VERSION}";
 
