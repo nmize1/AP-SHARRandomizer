8491ad34af17b6be76ca50ee150309b48278d082
Proddy <josh@zhbot.org>
2025-10-30 00:15:01 +0000

    fix/Fixed Radical's fucked car sounds
    - Audi TT has an invalid engine audio
    - Monorail has an invalid idle clip
    
    Now that these cars can be traffic, the fucked sounds need fixing

--- SHARMemory/SHARRandomizer/SHARRandomizerMod/Resources/HandleCarTuneSPT.lua
+++ SHARMemory/SHARRandomizer/SHARRandomizerMod/Resources/HandleCarTuneSPT.lua
@@ -1,9 +1,30 @@
-﻿local spt = ReadFile("/GameData/" .. GetPath())
-Output(spt)
+﻿local Path = GetPath()
+local GamePath = GetGamePath(Path)
 
-local redbrick = spt:match("create carSoundParameters named redbrick\r\n{(.-)}")
-for i=1,21 do
-    Output("create carSoundParameters named APCar" .. i .. "\r\n{")
-    Output(redbrick)
-    Output("}\r\n")
+local SPT = SPTParser.SPTFile(GamePath)
+
+for carSoundParameters in SPT:GetClasses("carSoundParameters") do
+	for method, index in carSoundParameters:GetMethods(true) do
+		local name = method.Name
+		if name == "SetEngineClipName" or name == "SetEngineIdleClipName" then
+			if method.Parameters[1] == "tt" then
+				method.Parameters[1] = "apu_car"
+			end
+		elseif name == "SetOverlayClipName" and method.Parameters[1] == "generator" then -- Fucking Radical and their broken ass Monorail
+			carSoundParameters:RemoveMethod(index)
+		end
+	end
 end
+
+local redbrick = SPT:GetClass("carSoundParameters", false, "redbrick")
+for i=1,21 do
+	local class = SPTParser.Class("carSoundParameters", "APCar" .. i)
+	
+	for method in redbrick:GetMethods() do
+		class:AddMethod(method.Name, method.Parameters, method.Option)
+	end
+	
+	SPT.Classes[#SPT.Classes + 1] = class
+end
+
+Output(tostring(SPT))
