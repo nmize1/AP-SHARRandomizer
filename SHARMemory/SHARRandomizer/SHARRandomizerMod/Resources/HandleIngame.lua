e95112c883ba51e1ad4df6b29ccda46433a7f57c
Proddy <josh@zhbot.org>
2026-02-15 19:45:35 +0000

    fix/3D Phonebooth chunk order
    Updated the `3DModel` group to go before the `Locked` group

--- SHARMemory/SHARRandomizer/SHARRandomizerMod/Resources/HandleIngame.lua
+++ SHARMemory/SHARRandomizer/SHARRandomizerMod/Resources/HandleIngame.lua
@@ -18,8 +18,9 @@ PhoneboothPageChunk:AddChunk(P3D.FrontendPure3DResourceP3DChunk("3Dmodel", 1, "p
 PhoneboothPageChunk:AddChunk(P3D.FrontendPure3DResourceP3DChunk("phonebg", 1, "pure3d\\_stubs\\dummy.p3d", "dummy", "Pedestal_Camera", "", ""), ForegroundLayerIndex + 1)
 PhoneboothPageChunk:AddChunk(P3D.FrontendPure3DResourceP3DChunk("rewardfg", 1, "pure3d\\rewardfg.p3d", "PurchaseScene", "Pedestal_Camera", "", ""), ForegroundLayerIndex + 2)
 
+local LockedGroupIndex = ForegroundLayerChunk:GetChunkIndexed(P3D.Identifiers.Frontend_Group, true, "Locked")
 local GroupChunk = P3D.FrontendGroupP3DChunk("3DModel", 0, 255)
-ForegroundLayerChunk:AddChunk(GroupChunk)
+ForegroundLayerChunk:AddChunk(GroupChunk, LockedGroupIndex)
 
 GroupChunk:AddChunk(P3D.FrontendPure3DObjectP3DChunk("RewardBG", 1, {X = 29, Y = 145}, {X = 400, Y = 300}, {X = P3D.FrontendPure3DObjectP3DChunk.Justifications.Left, Y = P3D.FrontendPure3DObjectP3DChunk.Justifications.Top}, {A=255,R=255,G=255,B=255}, 0, 0, "phonebg"))
 GroupChunk:AddChunk(P3D.FrontendPure3DObjectP3DChunk("RewardFG", 1, {X = 29, Y = 145}, {X = 400, Y = 300}, {X = P3D.FrontendPure3DObjectP3DChunk.Justifications.Left, Y = P3D.FrontendPure3DObjectP3DChunk.Justifications.Top}, {A=255,R=255,G=255,B=255}, 0, 0, "rewardfg"))
